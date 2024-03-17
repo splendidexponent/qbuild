aaddcec0980fc390e23381155714ae5a6ed0dabf
Mohan Sandesh <588700+splendidexponent@users.noreply.github.com>
2024-03-17 18:24:10 +0530

    Initial version of create table

--- test/index.js
+++ test/index.js
@@ -1,7 +1,30 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
+const qbuild = require('../index.js');
 
-test('synchronous passing test', (t) => {
-  // This test passes because it does not throw an exception.
-  assert.strictEqual(1, 1);
+test('disconnects the database', ()=>{
+  qbuild.connect();
+  qbuild.disconnect();
+});
+
+test('connects to the database', ()=>{
+  qbuild.connect();
+});
+
+test('creates a table', async ()=>{
+  const success = await qbuild.createTable('Hello', (table)=>{
+    table.string('name');
+  });
+
+  assert.strictEqual(success, true);
+  assert.strictEqual(qbuild.lastStatement, "CREATE TABLE Hello ( name string NOT NULL );");
+});
+
+test('fails to create a table', async ()=>{
+  const fail = await qbuild.createTable('Hello', (table)=>{
+    table.string('name');
+  });
+
+  assert.strictEqual(fail, false);
+  assert.strictEqual(qbuild.lastErrorMessage, "SQLITE_ERROR: table Hello already exists");
 });
